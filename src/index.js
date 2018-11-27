99fdaf72d24bd2f04ae64ce198bff8117e62c445
ManiaU <yumingyuan001@lianjia.com>
2018-11-27 15:54:09 +0800

    upgrade to react-V17

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './container/App';
-render(<App />,document.getElementById('app'));
+ReactDOM.render(<App />,document.getElementById('app'));
